2350eee29e6207a4f2c28185cb03c4eec77495db
SFontal <sergiot.fontal@gmail.com>
2023-01-23 04:01:49 +0100

    Change colors and add total cells showed on screen

--- src/js/modules/createCells/createCells.js
+++ src/js/modules/createCells/createCells.js
@@ -1,10 +1,10 @@
-const createCells = (mode) => {
-  if (!mode) {
+const createCells = (totalCells) => {
+  if (!totalCells) {
     return [false, false, false, false, false, false, false, false, false];
   }
 
   const cellRandomStatus = () => {
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < totalCells; i++) {
       const randomStatus = Math.floor(Math.random() * 2);
       const cellStatus = randomStatus === 1;
       arrayCells.push(cellStatus);
